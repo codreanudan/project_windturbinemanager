2dea0c9c69decc52e02cedcf4db77d7eb1e1493f
CodreanuDan <codreanud14@gmail.com>
2025-05-18 16:13:27 +0300

    update for railway url in settings.py

--- wind_turbine_monitor/wind_turbine_monitor/settings.py
+++ wind_turbine_monitor/wind_turbine_monitor/settings.py
@@ -127,7 +127,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# added for static files and railway settings
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -139,3 +145,4 @@ LOGIN_REDIRECT_URL = "home"   # Redirect to home page after login
 LOGOUT_REDIRECT_URL = "home"  # Redirect to home page after logout
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
