c61a0f0093b947ae8a4b5ad99d85aff4250eb320
CodreanuDan <codreanud14@gmail.com>
2025-05-24 23:42:32 +0300

    update - added PUT and DELETE methodds

--- wind_turbine_monitor/turbines/models.py
+++ wind_turbine_monitor/turbines/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-
-# Create your models here.
+from django.utils import timezone
 
 class WindTurbine(models.Model):
     """Model representing a wind turbine."""
@@ -10,5 +9,26 @@ class WindTurbine(models.Model):
     installation_date = models.DateField()
     capacity_kw = models.FloatField(help_text="Puterea instalată în kW")
     
+    # Câmpuri mentenanță
+    last_maintenance_date = models.DateField(null=True, blank=True)
+    maintenance_done = models.BooleanField(default=False)
+    
+    # Exemplu output instantaneu (putere instantanee)
+    instant_output_kw = models.FloatField(default=0.0)
+
     def __str__(self):
         return f"{self.name} ({self.latitude}, {self.longitude})"
+    
+    def maintenance_overdue(self):
+        """
+        Returns True if maintenance is overdue.
+        Maintenance is considered overdue if:
+        - Maintenance has not been done yet.
+        - Last maintenance date is not set.
+        """
+        if not self.maintenance_done:
+            return True
+        if not self.last_maintenance_date:
+            return True
+        # Dacă ultima revizie a fost făcută cu mai mult de X zile în urmă (de exemplu, 365 zile)
+        return (timezone.now().date() - self.last_maintenance_date).days > 7
--- wind_turbine_monitor/turbines/serializers.py
+++ wind_turbine_monitor/turbines/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 from .models import WindTurbine
+from datetime import date
 
 class WindTurbineSerializer(serializers.ModelSerializer):
     """Serializer for the WindTurbine model."""
+    maintenance_overdue = serializers.SerializerMethodField()
     class Meta:
         """Meta class for the WindTurbineSerializer."""
         model = WindTurbine
         fields = '__all__'
+        read_only_fields = ('id',)
+        
+
+    def get_maintenance_overdue(self, obj):
+        if not obj.last_maintenance_date:
+            return False  
+        if date.today() > obj.last_maintenance_date and not obj.maintenance_done:
+            return True
+        return False
